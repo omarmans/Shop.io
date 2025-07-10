91102e25a2b311b39f816f5966dfa5f1d36fc995
Omar Bayoumi <obayoumi@getgroup.com>
2025-07-10 17:29:24 +0300

    fix products link

--- src/app/pages/homepage/browse/browse.component.ts
+++ src/app/pages/homepage/browse/browse.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-browse',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.scss',
 })
